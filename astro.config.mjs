58c30c4f633c289d75d0f2d6d84450af2996005f
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-08-21 17:51:10 -0700

    Update deployment to kidcali.co

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kidcalico.github.io',
-  base: '/kid-calico'
+  site: 'https://kidcalico.github.io/'
 });
